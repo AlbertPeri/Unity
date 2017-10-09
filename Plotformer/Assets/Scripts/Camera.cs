a01682cf9708f4b71a7a43a4db3c1d608a888aa3
Альберт <albert.brod@yandex.ru>
2017-10-09 18:14:50 +0300

    Add files via upload

---
+++ Plotformer/Assets/Scripts/Camera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+	public GameObject catPerson;
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		transform.position = new Vector3 (
+			catPerson.transform.position.x,
+			catPerson.transform.position.y,
+			transform.position.z);
+			
+}
+}
---
+++ Plotformer/Assets/Scripts/HeroScripts.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroScripts : MonoBehaviour {
+	public float speed = 5f;
+	public Rigidbody2D myRigit;
+	public int scores =0;
+	void Start () {
+		myRigit = GetComponent<Rigidbody2D> ();
+	}
+	
+
+	void Update () {
+		
+		float xMotion = Input.GetAxis ("Horizontal") * speed;
+		float yMotion = Input.GetAxis ("Vertical") * speed;
+		myRigit.AddForce (new Vector2(xMotion, yMotion));
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision){
+		if (collision.gameObject.tag == "gem") {
+			scores++;
+			Destroy (collision.gameObject);
+			Debug.Log (scores);
+		}
+	}
+}
